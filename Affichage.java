7bc9342f379682adf903328d638f0fc99b9049be
christopher.cabantous <christopher.cabantous@devisubox.com>
2020-04-04 12:11:53 +0200

    Affichage des parametre

---
+++ Affichage.java
@@ -0,0 +1,9 @@
+public class Affichage {
+
+    public static void main(String arguments[]) {
+
+        for (int i = 0; i < arguments.length; i++) {
+            System.out.println("Voici l'argument " + i + ": " + arguments[i]);
+        }
+    }
+}
